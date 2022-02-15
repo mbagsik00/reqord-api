25357378c24c1b3b24f57f8ebcc9d410826403e7
Melvin Bagsik <mbagsik00@gmail.com>
2022-02-15 20:34:40 +1300

    Add helmet, cors, and csurf security

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // TODO: Add rate limiting
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.use(csurf());
 
   const config = new DocumentBuilder()
     .setTitle('Reqord')
@@ -18,6 +23,7 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
